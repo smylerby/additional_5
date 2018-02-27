3c9c33ecbbe098fb09918291359205184c3e3f6e
Rustam Shorov <srmparusa@gmail.com>
2018-02-27 18:36:53 +0100

    More 50%, but i'll try better

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = new Array();
+    let len = str.length - 1;
+    for (let i in str) {
+        if (str[i] === '(' || str[i] === '{' || str[i] === '[') {
+            stack.push(str[i])
+        }
+        else if (str[i] === ')' || str[i] === '}' || str[i] === ']') {
+            if (stack.length === 0) {
+                return false
+            }
+            let last = stack.pop()
+            if ((last === '(' && str[i] !== ')') || (last === '{' && str[i] !== '}') || (last === '[' && str[i] !== ']')) {
+                return false
+            }
+        }
+        if (i == len) {
+            if (stack.length === 0){return true}
+            else {return false}
+        }
+    }
 }
