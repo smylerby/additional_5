a37485f8d36bed4484451f8a6e461b2df6f83c3d
Rustam Shorov <srmparusa@gmail.com>
2018-03-01 16:32:52 +0100

    add config

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-    let stack = new Array();
     let len = str.length - 1;
-    for (let i in str) {
-        if (str[i] === '(' || str[i] === '{' || str[i] === '[') {
-            stack.push(str[i])
-        }
-        else if (str[i] === ')' || str[i] === '}' || str[i] === ']') {
-            if (stack.length === 0) {
-                return false
+    let stack = [];
+    for (let i in str){
+        for (let y in bracketsConfig){
+
+            if (str[i] === '|' && bracketsConfig[y][0] === '|'){
+
+                if (stack.indexOf('|') != -1){
+                    let top = stack.pop();
+                    if (top !== '|'){
+                    return false}
+                }
+                else {
+                    stack.push(str[i]);
+                }
             }
-            let last = stack.pop()
-            if ((last === '(' && str[i] !== ')') || (last === '{' && str[i] !== '}') || (last === '[' && str[i] !== ']')) {
-                return false
+            
+            else if (str[i] === bracketsConfig[y][0]){
+                stack.push(str[i]);
+            }
+            else if (str[i] === bracketsConfig[y][1] ){
+                if (stack.length === 0) {
+                    return false;}
+
+                let top = stack.pop();
+                if (top !== bracketsConfig[y][0]){
+                    return false}
             }
         }
+
         if (i == len) {
             if (stack.length === 0){return true}
             else {return false}
